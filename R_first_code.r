8d958a8600c9c0541431174f51572490ade98bb3
emmaciavoni <92572834+emmaciavoni@users.noreply.github.com>
2021-10-22 10:55:14 +0200

    Update R_first_code.r

--- R_first_code.r
+++ R_first_code.r
@@ -16,3 +16,24 @@ plot(water, fishes)
 # a table in R is called data frame
 
 streams <- data.frame(water, fishes)
+
+# from now on, we are going to import and/or export data
+setwd("C:/Users/Emma/Desktop/lab/")
+
+# Let's export our table 
+write.table(streams, file="my_first_table.txt")
+
+# Some colleagues did send us a table How to import it in R?
+read.table("my_first_table.txt")
+# let's assign it to an object inside R 
+emmatable <- read.table("my_first_table.txt")
+# the first statistics for lazy beautiful people
+summary(emmatable) 
+
+# Marta does not like water 
+# Marta only wants to get info on fishes 
+summary(emmatable$fishes)
+
+# histograms
+hist(emmatable$fishes)
+hist(emmatable$water)
