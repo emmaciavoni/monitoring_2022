46d78a558d868f1805d9f77f65eea9319c743892
emmaciavoni <92572834+emmaciavoni@users.noreply.github.com>
2021-11-08 10:56:19 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -10,8 +10,10 @@ library(raster)
 setwd("C:/Users/Emma/Desktop/lab/")
 
 # we are going to import satellite data 
+# to do that we use the "brick" function, which imports the layers all together
+# we assign an object to the function
 l2011 <- brick("p224r63_2011.grd") 
-# objects cannot be numbers
+# objects cannot be numbers (I made a mistake, calling the object "12011")
 # l2011 is our satellite image
 # landsat has seven layers
 
@@ -73,3 +75,37 @@ plot(l2011$B1_sre, col=clb)
 plot(l2011$B2_sre, col=clg)
 
 
+# let's plot only the blue band
+plot(l2011$B1_sre)
+clb <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+plot(l2011$B1_sre, col=clb)
+
+
+# excercise: plot the first four bands
+# we have to make the colorRampPalette for the red band and nir
+
+clr <- colorRampPalette(c("dark red", "red", "pink"))(100)
+plot(l2011$B3_sre, col=clr)
+
+clnir <- colorRampPalette(c("red", "orange", "yellow"))(100)
+plot(l2011$B4_sre, col=clnir)
+
+par(mfrow=c(2,2))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+plot(l2011$B3_sre, col=clr)
+plot(l2011$B4_sre, col=clnir)
+
+# close the plot with dev.off
+dev.off()
+
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin") # natural colors
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin") # false colors
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin") # false colors
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin") # false colors
+
+par(mfrow=c(2,2))
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin") # natural colors
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin") # false colors
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin") # false colors
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin") # false colors
