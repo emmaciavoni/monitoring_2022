4e5c7de996a8467751c1fc771ec09da85b747964
emmaciavoni <92572834+emmaciavoni@users.noreply.github.com>
2021-11-29 11:01:54 +0100

    Update R_code_chemica_cycling.r

--- R_code_chemica_cycling.r
+++ R_code_chemica_cycling.r
@@ -36,3 +36,15 @@ cl <- colorRampPalette(c('red','orange','yellow'))(100)
 plot(EN_stack, col=cl)
 
 # let's plot only the first image of the stack
+plot(EN_stack$EN_0001, col=cl)
+
+# difference
+ENdif <-EN_stack$EN_0001 - EN_stack$EN_0013
+cldif <- colorRampPalette(c('blue','white','red'))(100)
+plot(ENdif, col=cldif)
+
+# the highest change in the sets are in red 
+# the blue part mantain the same amount of NO2 
+
+# automated processing Source Function 
+source("R_code_automatic_script.txt")
