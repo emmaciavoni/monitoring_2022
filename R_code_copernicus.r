e86571fd6750c3d8b01c550f9c27efdfd7bad0a5
emmaciavoni <92572834+emmaciavoni@users.noreply.github.com>
2021-12-17 10:41:10 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -9,7 +9,7 @@ library(ggplot2)
 setwd("C:/Users/Emma/Desktop/lab/copernicus")
 
 
-snow20211214 <- raster("c_gls_SCE500_202112140000_CEURO_MODIS_V1.0.1.nc")
+snow20211214 <- raster("c_gls_SCE_202112140000_NHEMI_VIIRS_V1.0.1.nc")
 snow20211214
 
 plot(snow20211214)
@@ -42,3 +42,65 @@ ggplot() +
 geom_raster(snow20211214, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) +
 scale_fill_viridis(option="cividis") +
 ggtitle("cividis palette")
+
+-----# day 3
+library(ncdf4) 
+library(raster)
+library(viridis)
+library(RStoolbox) 
+library(ggplot2)
+library(patchwork)
+# set the working directory
+setwd("C:/Users/Emma/Desktop/lab/copernicus")
+
+# lapply function for uploading both data at the same time
+# the pattern "SCE" is the one in common between the two files
+rlist <- list.files (pattern="SCE")
+rlist
+
+list_rast <- lapply(rlist, raster)
+list_rast
+
+snowstack <- stack(list_rast)
+snowstack
+
+ssummer <- snowstack$Snow.Cover.Extent.1
+ssummer
+swinter <- snowstack$Snow.Cover.Extent.2
+swinter
+
+p1 <- ggplot() +
+geom_raster(ssummer, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.1)) +
+scale_fill_viridis(option="viridis") +
+ggtitle("Snow cover during summer")
+
+p2 <- ggplot() +
+geom_raster(swinter, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.2)) +
+scale_fill_viridis(option="viridis") +
+ggtitle("Snow cover during freezing winter")
+
+# let's patchwork them together
+p1/p2
+
+# crop function
+# you can crop an image on a certain area 
+# longitude from 0 to 20 
+# latitude from 30 to 50 
+
+ext <- c(0, 20, 30, 50) 
+# stack_cropped, this will crop the whole stack, and then 
+ssummer_cropped <- crop(ssummer, ext)
+summer_cropped
+swinter_cropped <- crop(swinter, ext)
+
+p1 <- ggplot() +
+geom_raster(ssummer_cropped, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.1)) +
+scale_fill_viridis(option="viridis") +
+ggtitle("Snow cover during summer")
+
+p2 <- ggplot() +
+geom_raster(swinter_cropped, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.2)) +
+scale_fill_viridis(option="viridis") +
+ggtitle("Snow cover during freezing winter")
+
+p1/p2
