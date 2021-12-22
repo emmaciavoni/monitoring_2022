c6038d48a4c22d186c84622197c4589ce87ecf15
emmaciavoni <92572834+emmaciavoni@users.noreply.github.com>
2021-12-22 12:41:42 +0100

    Update R_code_sdm.r

--- R_code_sdm.r
+++ R_code_sdm.r
@@ -1,1 +1,18 @@
+# R code for species distribution modelling, namely the ditribution of individuals
+install.packages("sdm")
+library(sdm)
+# sdm = species distribution modelling 
+# An extensible framework for developing species distribution models using individual and community-based approaches
+# generate ensembles of models, evaluate the models, and predict species potential distributions in space and time
 
+# recalling the libraries 
+library(raster)
+library(rgdal)
+
+# system.file function shows all the files in a certain package 
+
+file <- system.file("external/species.shp", package="sdm")
+
+species <- shapefile(file)
+
+plot(species, pch=19, col="red")
