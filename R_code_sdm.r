c590839e5b43dc2ccee086daf160de656c3100b7
emmaciavoni <92572834+emmaciavoni@users.noreply.github.com>
2022-01-10 10:54:07 +0100

    Update R_code_sdm.r

--- R_code_sdm.r
+++ R_code_sdm.r
@@ -6,13 +6,72 @@ library(sdm)
 # generate ensembles of models, evaluate the models, and predict species potential distributions in space and time
 
 # recalling the libraries 
-library(raster)
-library(rgdal)
+library(raster) # predictors
+library(rgdal) # species
 
 # system.file function shows all the files in a certain package 
 
+# species data 
 file <- system.file("external/species.shp", package="sdm")
-
-species <- shapefile(file)
+file
+species <- shapefile(file) 
+species
+# is the correspondant function of raster
 
 plot(species, pch=19, col="red")
+# The pch in R defines the point symbols in the functions plot() and lines()
+# The pch stands for plot character. The pch contains numeric values rangers from 0 to 25 or character symbols (“+”, “.”, “;”, etc.) specifying in symbols (or shapes)
+
+---------# day 2 
+# SQL is a database query language - a language designed specifically for interacting with a database
+# how many occurrences are there?
+# we want to see how many "ones" there are
+presences <- species[species$Occurrence == 1,]
+# the comma in SQL language means "stop the query"
+# there are 94 occurrences 
+absences <- species[species$Occurrence == 0,]
+# 106 absences 
+
+# let's plot the data
+plot(species, pch=19)
+plot(presences, pch=19, col="blue")
+# we want to make a single plot with presences and absence 
+# how to add? we use the "points" function
+points(absences, pch=19, col="red")
+
+# plot the probability to find a species 
+# let's look at the predictors
+# predicotrs: look at the path
+path <- system.file("external", package="sdm")
+path
+
+# list the predictors 
+lst <- list.files(path, pattern="asc")
+lst
+
+# use lapply function with the raster function 
+
+lst <- list.files(path, pattern="asc$", full.names=T)
+lst
+
+# stack
+preds <- stack(lst)
+preds
+
+# plot preds 
+cl <- colorRampPalette(c("blue", "orange", "red", "yellow")) (100)
+plot(preds, col=cl)
+
+plot(preds$elevation, col=cl)
+points(presences, pch=19)
+
+plot(preds$temperature, col=cl)
+points(presences, pch=19)
+
+plot(preds$vegetation, col=cl)
+points(presences, pch=19)
+
+plot(preds$precipitation, col=cl)
+points(presences, pch=19)
+
+# model 
