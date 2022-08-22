16bfea4a390120d8cff9b9c97012a2be9cf05da7
emmaciavoni <92572834+emmaciavoni@users.noreply.github.com>
2022-08-22 18:09:19 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -5,13 +5,13 @@ library(viridis)
 library(RStoolbox) 
 library(ggplot2)
 
-fire2022043 <- raster("c_gls_BA300_202204300000_GLOBE_S3_V1.2.1.nc")
-fire2022043
+fire20210831 <- raster("c_gls_BA300_202108310000_GLOBE_S3_V1.2.1.nc")
+fire20210831
 
-plot(fire2022043)
+plot(fire20210831)
 
 cl <- colorRampPalette (c('red','orange','yellow'))(100)
-plot(fire2022043, col=cl)
+plot(fire20210831, col=cl)
 
 
 rlist <- list.files (pattern="BA300")
@@ -20,3 +20,6 @@ rlist
 
 list_rast <- lapply(rlist, raster)
 list_rast
+
+cl <- colorRampPalette (c('black','red','white'))(100)
+plot(fire20210831, col=cl)
