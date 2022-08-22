87eacf4a2536744707d71b47dde61afb0088a3aa
emmaciavoni <92572834+emmaciavoni@users.noreply.github.com>
2022-08-22 17:13:12 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -9,3 +9,6 @@ fire2022043 <- raster("c_gls_BA300_202204300000_GLOBE_S3_V1.2.1.nc")
 fire2022043
 
 plot(fire2022043)
+
+cl <- colorRampPalette (c('red','orange','yellow'))(100)
+plot(fire2022043, col=cl)
