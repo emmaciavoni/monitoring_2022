2f3ec36d027afe6c0720cc143e19efff3a5dc967
emmaciavoni <92572834+emmaciavoni@users.noreply.github.com>
2023-06-18 17:18:18 +0200

    Update test.r

--- test.r
+++ test.r
@@ -1,3 +1,5 @@
 # prova per capire delle cose
 # dovrei inserire la working directory e poi inserire le library che mi servono
 setwd("C:/Users/Emma/Desktop/exam/")
+library(raster) # predictors
+library(rgdal) # species
